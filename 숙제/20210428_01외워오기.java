1569c77b401919bacef7704190d20a38639309a2
2miri <83326164+2miri@users.noreply.github.com>
2021-04-28 22:22:47 +0900

    Create 20210428_01외워오기.java

---
+++ 숙제/20210428_01외워오기.java
@@ -0,0 +1,26 @@
+/*
+
+
+	< 조건문(conditional statements) >
+	- if, switch
+	
+	< 반복문 (loop statements) >
+	- while, do-while, for
+	
+	< 분기문 (branching statement) >
+	- break, continue, return,
+	  throw, ...
+	  
+	break :
+		자신이 속한 switch, while, do-while, for를 종료시킨다.
+		~> 반복 종료
+		
+	continue : 
+		자신이 속한 while, do-while, for의 종속문장을 1회 건너뜀
+		~> 반복 유지. 그 순간만 skip
+		
+	return : 
+		자신이 속한 함수(=메서드)를 종료시킨다.
+		예) 메인메서드의 return 은 메인메서드를 종료시킨다.
+	
+ */
